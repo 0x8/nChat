d1fde5cd5e77dc384f3c78399d0971495fb977ee
nullp0inter <null>
2017-03-28 10:57:09 -0400

    initial commit

---
+++ nchat_client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Ian Guibas (nullp0inter)
+# nChat seeks to provide an encrypted CLI
+# IM platform between two peers.
+
+import socket
+import ssl          # If method = SSL
+
+
