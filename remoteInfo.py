c28139d8c49d1a4ad64010aaa9ef5c7135aea6a7
nullp0inter <null>
2017-04-11 20:51:34 -0400

    A wrapper around the remote servers info

---
+++ remoteInfo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+Ian Guibas
+
+This module contains a class meant to hold details about each remote connection.
+This is just to make life easier when working with connections and maintain some
+sort of memory
+'''
+
+class remoteInfo:
+
+    def __init__():
+        self.HOST = None
+        self.PORT = None
+        self.username = None
+        self.password = None
+        self.publicKey = None
+        self.key = None
+        self.IV = None
