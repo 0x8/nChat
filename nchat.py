966ab01b3061df87833c7969384abe3d39f21ad3
nullp0inter <null>
2017-04-15 03:00:40 -0400

    Added debug flag and fixed logging behavior

--- nchat.py
+++ nchat.py
@@ -12,31 +12,46 @@ import Config
 import client
 import logging
 
+# Allow color log debug mode
+DEBUG = True
 
 if __name__ == '__main__':
     # Start logging
-    try:                                                                        
-        log_dateformat = '%Y-%m-%dT%H:%M:%S'
-        log_format  = '%(asctime)s | %(levelname)s %(module)s:%(funcName)s'
-        log_format += ':%(lineno)d [%(process)d] | %(message)s'
-        import coloredlogs                                                      
-    except ImportError:                                                         
-        logging.basicConfig(                                                    
-            filename='logs/nchat.log',                                          
+                                                                        
+    log_dateformat = '%Y-%m-%dT%H:%M:%S'
+    log_format  = '%(asctime)s | %(levelname)s %(module)s:%(funcName)s'
+    log_format += ':%(lineno)d [%(process)d] | %(message)s'
+    
+    # If debug is on, use colored logs if available
+    if DEBUG:
+        
+        try:
+                import coloredlogs                                                      
+                coloredlogs.install(                                                    
+                    filename='logs/nchat.log',                                          
+                    level=logging.DEBUG,                                                 
+                    datefmt=log_dateformat,                                             
+                    field_styles = {                                                    
+                        'asctime': {'color': 'green'},                                  
+                        'levelname': {'color': 'black', 'bold': True},                  
+                        'module': {'color': 'yellow'},                                  
+                        'process':{'color':'magenta'}},                                 
+                    fmt = log_format) 
+        
+        except ImportError:                                                         
+            logging.basicConfig(                                                    
+                filename='logs/nchat.log',                                          
+                level=logging.DEBUG,                                                 
+                format=log_format,                                                  
+                datefmt=log_dateformat)                                             
+    
+    # If DEBUG is set to False (default) all logging goes to a file
+    else:                                                                       
+        logging.basicConfig(
+            filename='logs/nchat.log',
             level=logging.INFO,                                                 
             format=log_format,                                                  
-            datefmt=log_dateformat)                                             
-    else:                                                                       
-        coloredlogs.install(                                                    
-            filename='logs/nchat.log',                                          
-            level=logging.INFO,                                                 
-            datefmt=log_dateformat,                                             
-            field_styles = {                                                    
-                'asctime': {'color': 'green'},                                  
-                'levelname': {'color': 'black', 'bold': True},                  
-                'module': {'color': 'yellow'},                                  
-                'process':{'color':'magenta'}},                                 
-            fmt = log_format) 
+            datefmt=log_dateformat)
     
     logging.info('Started logging')
     logging.info('Started main chat program')
@@ -48,10 +63,11 @@ if __name__ == '__main__':
     logging.info('Starting server on {0}:{1}'.format(serverInfo.HOST,
                                                      serverInfo.PORT))
     # Start client and server 
-    threadedServer.start_server(serverInfo)
+    ts = threadedServer
+    ts.start_server(serverInfo)
     
     logging.info('Server started on {0}:{1}'.format(serverInfo.HOST,
                                                         serverInfo.PORT))
     
     logging.info('Starting client...')
-    client.start_client(serverInfo)
+    client.start_client(serverInfo,ts)
